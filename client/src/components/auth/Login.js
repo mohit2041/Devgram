cbdcb8cb00459957335b7fab5c6917c9e8637d1d
mohit <mohitbiddu1@gmail.com>
2021-01-29 09:23:26 +0530

    home,login,signUp page added - react part

---
+++ client/src/components/auth/Login.js
@@ -0,0 +1,64 @@
+import React, { Fragment, useState } from "react";
+import { Link } from "react-router-dom";
+
+const Login = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const { email, password } = formData;
+
+  const onChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log("success!");
+  };
+
+  return (
+    <Fragment>
+      <h1 className="large text-primary">Sign in</h1>
+
+      <p className="lead">
+        <i className="fas fa-user"></i> Sign in Your Account
+      </p>
+
+      <form className="form" onSubmit={onSubmit}>
+        <div className="form-group">
+          <input
+            type="email"
+            placeholder="Email Address"
+            name="email"
+            value={email}
+            onChange={onChange}
+          />
+        </div>
+
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={password}
+            onChange={onChange}
+            minLength="6"
+          />
+        </div>
+
+        <input type="submit" className="btn btn-primary" value="Login" />
+      </form>
+
+      <p className="my-1">
+        Already have an account? <Link to="/register">Log In</Link>
+      </p>
+    </Fragment>
+  );
+};
+
+export default Login;
